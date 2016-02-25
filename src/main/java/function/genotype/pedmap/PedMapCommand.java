53a3411b8a24a3daa125022e8d03c3362ebc9244
Nick Ren <zhong.ren@hotmail.com>
2016-02-25 10:18:59 -0500

    --ped-map function , get rid of combined alleles feature, output variant id only , simplified code

--- src/main/java/function/genotype/pedmap/PedMapCommand.java
+++ src/main/java/function/genotype/pedmap/PedMapCommand.java
@@ -11,12 +11,8 @@ public class PedMapCommand {
 
     public static boolean isPedMap = false;
     public static boolean isVariantIdOnly = false;
-    public static boolean isCombineMultiAlleles = false;
     public static boolean isEigenstrat = false;
-    public static boolean useGroup = false;
-    public static String GroupType = "control";
     public static String pedMapPath = "";
-    public static double dmaf = 0.5;   // 0.01;
 
     public static void initOptions(Iterator<CommandOption> iterator) {
         CommandOption option;
@@ -25,8 +21,6 @@ public class PedMapCommand {
             option = (CommandOption) iterator.next();
             if (option.getName().equals("--variant-id-only")) {
                 isVariantIdOnly = true;
-            } else if (option.getName().equals("--combine-multiple-alleles")) {
-                isCombineMultiAlleles = true;
             } else if (option.getName().equals("--eigenstrat")) {
                 isEigenstrat = true;
             } else {
