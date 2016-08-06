9bbab8c13aa7fc7e56e255ea4abe152e384bc5d1
Nick Ren <zhong.ren@hotmail.com>
2016-08-05 21:31:00 -0400

    trunk - 6.4.3 or newer

--- src/main/java/global/Data.java
+++ src/main/java/global/Data.java
@@ -8,7 +8,7 @@ public class Data {
 
     // software info
     public static final String AppTitle = "ATAV (Analysis Tool for Annotated Variants)";
-    public static String version = "6.4.2";
+    public static String version = "trunk";
     public static final String year = "2016";
     public static final String leadDeveloper = "Nick Ren";
     public static final String pipelineDeveloper = "Joshua Bridgers, Brett Copeland, Raghavendra Padmanabhan";
