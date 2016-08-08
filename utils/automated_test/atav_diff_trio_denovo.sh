d2450eeeedf9d67f7c291b6da8d0b66f61ee810c
Nick Ren <zhong.ren@hotmail.com>
2016-08-08 16:08:09 -0400

    take off checking noflag trio output from automated testing script

--- utils/automated_test/atav_diff_trio_denovo.sh
+++ utils/automated_test/atav_diff_trio_denovo.sh
@@ -1,5 +1,3 @@
 . atav_config_paths.sh
 
 diff $ATAV_OUTPUT_PATH/new/trio_denovo/trio_denovo_denovoandhom.csv $ATAV_OUTPUT_PATH/old/trio_denovo/trio_denovo_denovoandhom.csv > $ATAV_DIFF_PATH/denovoandhom.diff
-
-diff $ATAV_OUTPUT_PATH/new/trio_denovo/trio_denovo_denovoandhom_noflag.csv $ATAV_OUTPUT_PATH/old/trio_denovo/trio_denovo_denovoandhom_noflag.csv > $ATAV_DIFF_PATH/denovoandhom_noflag.diff
