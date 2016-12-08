d8031dcf855fa13b7c96984fb7b38452f6c8fd2e
Nick Ren <zhong.ren@hotmail.com>
2016-12-08 13:48:54 -0500

    implemented dragen version --list-sibling-comp-het

--- src/main/java/function/genotype/sibling/CompHetOutput.java
+++ src/main/java/function/genotype/sibling/CompHetOutput.java
@@ -1,20 +1,8 @@
 package function.genotype.sibling;
 
-import function.annotation.base.TranscriptManager;
 import function.genotype.base.CalledVariant;
 import function.variant.base.Output;
 import function.genotype.base.Sample;
-import global.Index;
-import function.external.evs.EvsManager;
-import function.external.exac.ExacManager;
-import function.external.genomes.GenomesManager;
-import function.external.gerp.GerpManager;
-import function.external.kaviar.KaviarManager;
-import function.external.knownvar.KnownVarManager;
-import function.external.mgi.MgiManager;
-import function.external.rvis.RvisManager;
-import function.external.subrvis.SubRvisManager;
-import function.external.trap.TrapManager;
 import utils.FormatManager;
 
 /**
@@ -34,58 +22,19 @@ public class CompHetOutput extends Output implements Comparable {
                 + "Child1 Trio Comp Het Flag,"
                 + "Child2,"
                 + "Child2 Trio Comp Het Flag,"
-                + "Gene Name,"
                 + initVarTitleStr("1")
                 + initVarTitleStr("2");
     }
 
     private static String initVarTitleStr(String var) {
-        String varTitle = "Variant ID,"
-                + "Variant Type,"
-                + "Rs Number,"
-                + "Ref Allele,"
-                + "Alt Allele,"
-                + GerpManager.getTitle()
-                + TrapManager.getTitle()
-                + "Is Minor Ref,"
-                + "Child1 Genotype,"
-                + "Child1 Samtools Raw Coverage,"
-                + "Child2 Genotype,"
-                + "Child2 Samtools Raw Coverage,"
-                + "Major Hom Case,"
-                + "Het Case,"
-                + "Minor Hom Case,"
-                + "Minor Hom Case Freq,"
-                + "Het Case Freq,"
-                + "Major Hom Ctrl,"
-                + "Het Ctrl,"
-                + "Minor Hom Ctrl,"
-                + "Minor Hom Ctrl Freq,"
-                + "Het Ctrl Freq,"
-                + "Missing Case,"
-                + "QC Fail Case,"
-                + "Missing Ctrl,"
-                + "QC Fail Ctrl,"
-                + "Case MAF,"
-                + "Ctrl MAF,"
-                + "Case HWE_P,"
-                + "Ctrl HWE_P,"
-                + EvsManager.getTitle()
-                + "Polyphen Humdiv Score,"
-                + "Polyphen Humdiv Prediction,"
-                + "Polyphen Humvar Score,"
-                + "Polyphen Humvar Prediction,"
-                + "Function,"
-                + "Transcript Stable Id,"
-                + "Is CCDS Transcript,"
-                + "Codon Change,"
-                + ExacManager.getTitle()
-                + KaviarManager.getTitle()
-                + KnownVarManager.getTitle()
-                + RvisManager.getTitle()
-                + SubRvisManager.getTitle()
-                + GenomesManager.getTitle()
-                + MgiManager.getTitle();
+        String varTitle = getVariantDataTitle()
+                + getAnnotationDataTitle()
+                + getExternalDataTitle()
+                + getGenotypeDataTitle()
+                + "Child1 GT,"
+                + "Child1 DP Bin,"
+                + "Child2 GT,"
+                + "Child2 DP Bin,";
 
         String[] list = varTitle.split(",");
 
@@ -105,52 +54,15 @@ public class CompHetOutput extends Output implements Comparable {
     public String getString(Sample child1, Sample child2) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append(calledVar.getVariantIdStr()).append(",");
-        sb.append(calledVar.getType()).append(",");
-        sb.append(calledVar.getRsNumberStr()).append(",");
-        sb.append(calledVar.getRefAllele()).append(",");
-        sb.append(calledVar.getAllele()).append(",");
-        sb.append(calledVar.getGerpScore());
-        sb.append(calledVar.getTrapScore());
-        sb.append(isMinorRef).append(",");
+        calledVar.getVariantData(sb);
+        calledVar.getAnnotationData(sb);
+        calledVar.getExternalData(sb);
+        getGenotypeData(sb);
+
         sb.append(getGenoStr(calledVar.getGT(child1.getIndex()))).append(",");
         sb.append(FormatManager.getDouble(calledVar.getDPBin(child1.getIndex()))).append(",");
         sb.append(getGenoStr(calledVar.getGT(child2.getIndex()))).append(",");
         sb.append(FormatManager.getDouble(calledVar.getDPBin(child2.getIndex()))).append(",");
-        sb.append(majorHomCount[Index.CASE]).append(",");
-        sb.append(genoCount[Index.HET][Index.CASE]).append(",");
-        sb.append(minorHomCount[Index.CASE]).append(",");
-        sb.append(FormatManager.getDouble(minorHomFreq[Index.CASE])).append(",");
-        sb.append(FormatManager.getDouble(hetFreq[Index.CASE])).append(",");
-        sb.append(majorHomCount[Index.CTRL]).append(",");
-        sb.append(genoCount[Index.HET][Index.CTRL]).append(",");
-        sb.append(minorHomCount[Index.CTRL]).append(",");
-        sb.append(FormatManager.getDouble(minorHomFreq[Index.CTRL])).append(",");
-        sb.append(FormatManager.getDouble(hetFreq[Index.CTRL])).append(",");
-        sb.append(genoCount[Index.MISSING][Index.CASE]).append(",");
-        sb.append(calledVar.getQcFailSample(Index.CASE)).append(",");
-        sb.append(genoCount[Index.MISSING][Index.CTRL]).append(",");
-        sb.append(calledVar.getQcFailSample(Index.CTRL)).append(",");
-        sb.append(FormatManager.getDouble(minorAlleleFreq[Index.CASE])).append(",");
-        sb.append(FormatManager.getDouble(minorAlleleFreq[Index.CTRL])).append(",");
-        sb.append(FormatManager.getDouble(hweP[Index.CASE])).append(",");
-        sb.append(FormatManager.getDouble(hweP[Index.CTRL])).append(",");
-        sb.append(calledVar.getEvsStr());
-        sb.append(calledVar.getPolyphenHumdivScore()).append(",");
-        sb.append(calledVar.getPolyphenHumdivPrediction()).append(",");
-        sb.append(calledVar.getPolyphenHumvarScore()).append(",");
-        sb.append(calledVar.getPolyphenHumvarPrediction()).append(",");
-        sb.append(calledVar.getEffect()).append(",");
-        sb.append(calledVar.getStableId()).append(",");
-        sb.append(TranscriptManager.isCCDSTranscript((calledVar.getStableId()))).append(",");
-        sb.append(calledVar.getHGVS_c()).append(",");
-        sb.append(calledVar.getExacStr());
-        sb.append(calledVar.getKaviarStr());
-        sb.append(calledVar.getKnownVarStr());
-        sb.append(calledVar.getRvis());
-        sb.append(calledVar.getSubRvis());
-        sb.append(calledVar.get1000Genomes());
-        sb.append(calledVar.getMgi());
 
         return sb.toString();
     }
