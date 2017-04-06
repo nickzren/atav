cd3936d94763c2f97bc71ffd819e3e15409654ec
Nick Ren <zhong.ren@hotmail.com>
2017-04-06 13:17:05 -0400

    keep code for carrier table block conversion

--- src/main/java/function/test/Test.java
+++ src/main/java/function/test/Test.java
@@ -39,7 +39,7 @@ public class Test extends AnalysisBase {
     @Override
     public void processDatabaseData() {
         try {
-            ConvertCalledVariant.run();
+//            ConvertCalledVariant.run();
         } catch (Exception ex) {
             Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         }
