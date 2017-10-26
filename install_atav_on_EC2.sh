ce8fd87d9156a4ebbc557d43b30c1d896aeaf17e
Nick Ren <zhong.ren@hotmail.com>
2017-10-26 14:24:45 -0400

    content clean

--- install_atav_on_EC2.sh
+++ install_atav_on_EC2.sh
@@ -2,18 +2,22 @@
 
 mkdir -p /home/ec2-user
 cd /home/ec2-user
+
 # Install git
 sudo yum -y install git
+
 # Install Maven
 wget http://apache.claz.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar xvzf apache-maven-3.3.9-bin.tar.gz
 rm apache-maven-3.3.9-bin.tar.gz*
 export PATH=/home/ec2-user/apache-maven-3.3.9/bin:$PATH
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
+
 #  setup java 8
 sudo yum remove java-1.7.0 -y 
 sudo yum install java-1.8.0-openjdk-devel -y
-export JAVA_HOME=/usr
+export JAVA_HOME=/
+
 # Install ADAM
 git clone -b dragen https://github.com/igm-team/atav.git
 cd /home/ec2-user
