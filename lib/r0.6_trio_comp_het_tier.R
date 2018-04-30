c4084f493e0041b58e5407e87cbb10605d2972d0
Nick Ren <zhong.ren@hotmail.com>
2018-04-30 16:11:30 -0400

    Updated new tier downstream script for trio analysis

---
+++ lib/r0.6_trio_comp_het_tier.R
@@ -0,0 +1,29 @@
+do_comp_het_tier <- function(comphetFile){
+    ### comphetFile is the fully qualified path to ATAV's denovo output
+    ### atavOutput is the directory to which ATAV will write the output files
+    source("/nfs/goldstein/software/atav_home/lib/r0.6_trio_filters.R")
+    atav.output = read.csv(comphetFile,na.strings="NA")
+    atavOutput <- dirname(comphetFile)
+    data <- Filter.by.CompHet.Flag(atav.output,'COMPOUND HETEROZYGOTE', strict = FALSE)
+    data <- Filter.for.compound.heterozygote(data)
+    write.csv(data, file = paste(atavOutput,"/compound.heterozygote.csv", sep=""))
+    data <- Filter.by.CompHet.Flag(atav.output,'POSSIBLY COMPOUND HETEROZYGOTE', strict = TRUE)  
+    poschet <- Filter.for.compound.heterozygote(data)
+    write.csv(poschet, file = paste(atavOutput,"/possibly.compound.heterozygote.csv", sep=""))
+    data <- Filter.by.CompHet.Flag(atav.output,'DENOVO WITH INHERITED VARIANT', strict = TRUE)  
+    dnmchet <- Filter.for.compound.heterozygote(data)
+    write.csv(dnmchet, file = paste(atavOutput,"/possibly.denovo.compound.heterozygote.csv", sep=""))
+
+    input.data = read.csv(comphetFile,na.strings="NA",stringsAsFactors=FALSE)
+
+    data <- Filter.by.CompHet.Flag(input.data,'COMPOUND HETEROZYGOTE', strict = FALSE)
+    data <- Filter.by.HemiHomo.Count(data,9, is.comphet = TRUE)
+    data <- Filter.for.tier2(data, is.comphet = TRUE)
+    write.csv(data, file = paste(atavOutput,"/compound.heterozygote_tier2.csv", sep=""))
+}
+
+### MAIN ###
+args <- commandArgs(T)
+comphet.comphetFile <- args[1]
+comphet.atavOutput <-args[2]
+do_comp_het_tier(comphet.comphetFile)
---
+++ lib/r0.6_trio_denovo_tier.R
@@ -0,0 +1,45 @@
+do_denovo_tier <- function(denovoFile){
+### denovoFile is the fully qualified path to ATAV's denovo output
+### atavOutput is the directory to which ATAV will write the output files
+###rm(list = ls())
+#### Test change 
+source("/nfs/goldstein/software/atav_home/lib/r0.6_trio_filters.R")
+atav.output = read.csv(denovoFile,na.strings="NA")
+atavOutput <- dirname(denovoFile)
+data <- Filter.by.Denovo.Flag(atav.output,'DE NOVO', strict = TRUE)
+data <- Filter.for.denovo(data)
+write.csv(data, file = paste(atavOutput,"/denovo.csv", sep=""))
+
+data <- Filter.by.Denovo.Flag(atav.output,'NEWLY HEMIZYGOUS', strict = TRUE)
+data <- rbind(data,Filter.by.Denovo.Flag(data,'COMPOUND DELETION', strict = TRUE))
+data <- Filter.for.hemizygous(data)
+write.csv(data, file = paste(atavOutput,"/hemizygous.csv", sep=""))
+
+data <- Filter.by.Denovo.Flag(atav.output,'NEWLY HOMOZYGOUS', strict = TRUE)
+data <- Filter.for.homozygous(data)
+write.csv(data, file = paste(atavOutput,"/homozygous.csv", sep=""))
+
+###rm(list = ls())
+input.data = read.csv(denovoFile,na.strings="NA",stringsAsFactors=FALSE)
+
+data <- Filter.by.Denovo.Flag(input.data,'DE NOVO', strict = FALSE)
+data <- Filter.by.Allele.Count(data,19)
+data <- Filter.for.tier2(data)
+write.csv(data, file = paste(atavOutput,"/denovo_tier2.csv", sep=""))
+
+data <- Filter.by.Denovo.Flag(input.data,'NEWLY HEMIZYGOUS', strict = FALSE)
+data <- rbind(data,Filter.by.Denovo.Flag(data,'COMPOUND DELETION', strict = TRUE))
+data <- Filter.by.HemiHomo.Count(data,9)
+data <- Filter.for.tier2(data)
+write.csv(data, file = paste(atavOutput,"/hemizygous_tier2.csv", sep=""))
+
+data <- Filter.by.Denovo.Flag(input.data,'NEWLY HOMOZYGOUS', strict = FALSE)
+data <- Filter.by.HemiHomo.Count(data,9)
+data <- Filter.for.tier2(data)
+write.csv(data, file = paste(atavOutput,"/homozygous_tier2.csv", sep=""))
+}
+### MAIN ###
+args <- commandArgs(T)
+denovo.denovoFile <- args[1]
+deno.atavOutput <-args[2]
+do_denovo_tier(denovo.denovoFile)
