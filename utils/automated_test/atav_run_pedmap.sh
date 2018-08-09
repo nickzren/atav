a0924c149d3911f955d8e89310bda3865ca21e12
Nick Ren <zhong.ren@hotmail.com>
2018-08-09 11:08:15 -0400

    added debug option --disable-timestamp-from-out-path

--- utils/automated_test/atav_run_pedmap.sh
+++ utils/automated_test/atav_run_pedmap.sh
@@ -1,5 +1,5 @@
 . atav_config_paths.sh
 
-atav_beta.sh --sample $ATAV_TEST_PATH/trio_sample.txt --effect $ATAV_TEST_PATH/function --region $ATAV_TEST_PATH/region --out $ATAV_OUTPUT_PATH/new/trio_sample.txtmap --ped-map --min-coverage 10 --filter PASS
+atav_beta.sh --disable-timestamp-from-out-path --sample $ATAV_TEST_PATH/trio_sample.txt --effect $ATAV_TEST_PATH/function --region $ATAV_TEST_PATH/region --out $ATAV_OUTPUT_PATH/new/pedmap --ped-map --min-coverage 10 --filter PASS
 
-atav_latest.sh --sample $ATAV_TEST_PATH/trio_sample.txt --effect $ATAV_TEST_PATH/function --region $ATAV_TEST_PATH/region --out $ATAV_OUTPUT_PATH/old/trio_sample.txtmap --ped-map --min-coverage 10 --filter PASS
+atav_latest.sh --disable-timestamp-from-out-path --sample $ATAV_TEST_PATH/trio_sample.txt --effect $ATAV_TEST_PATH/function --region $ATAV_TEST_PATH/region --out $ATAV_OUTPUT_PATH/old/pedmap --ped-map --min-coverage 10 --filter PASS
--- utils/automated_test/atav_run_site_coverage_comparison.sh
+++ utils/automated_test/atav_run_site_coverage_comparison.sh
@@ -1,5 +1,5 @@
 . atav_config_paths.sh
 
-atav_beta.sh --site-coverage-comparison --sample $ATAV_TEST_PATH/trio_sample.txt --gene-boundary $ATAV_TEST_PATH/coverage_region --out $ATAV_OUTPUT_PATH/new/site_coverage_comparison --min-coverage 10
+atav_beta.sh --disable-timestamp-from-out-path --site-coverage-comparison --sample $ATAV_TEST_PATH/trio_sample.txt --gene-boundary $ATAV_TEST_PATH/coverage_region --out $ATAV_OUTPUT_PATH/new/site_coverage_comparison --min-coverage 10
 
-atav_latest.sh --site-coverage-comparison --sample $ATAV_TEST_PATH/trio_sample.txt --gene-boundary $ATAV_TEST_PATH/coverage_region --out $ATAV_OUTPUT_PATH/old/site_coverage_comparison --min-coverage 10
+atav_latest.sh --disable-timestamp-from-out-path --site-coverage-comparison --sample $ATAV_TEST_PATH/trio_sample.txt --gene-boundary $ATAV_TEST_PATH/coverage_region --out $ATAV_OUTPUT_PATH/old/site_coverage_comparison --min-coverage 10
