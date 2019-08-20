48a271133e095a6841c76c15c65735ad0769ae1c
Nick <zhong.ren@hotmail.com>
2019-08-20 15:37:24 -0400

    annodb_pgl

--- src/main/java/global/Data.java
+++ src/main/java/global/Data.java
@@ -8,12 +8,12 @@ public class Data {
 
     // software info
     public static final String APP_NAME = "ATAV (Analysis Tool for Annotated Variants)";
-    public static String VERSION = "6.6.6";
+    public static String VERSION = "annodb_pgl";
     public static String userName = System.getProperty("user.name");
 
     // system config file path
-    public static final String SYSTEM_CONFIG = "/nfs/goldstein/software/config/atav.system.config.properties";
-    public static final String SYSTEM_CONFIG_FOR_DEBUG = "/nfs/goldstein/software/config/atav.debug.system.config.properties";
+    public static final String SYSTEM_CONFIG = "/nfs/goldstein/software/config/atav.pgl.system.config.properties";
+    public static final String SYSTEM_CONFIG_FOR_DEBUG = "/nfs/goldstein/software/config/atav.pgl.system.config.properties";
 
     // atav home path
     public static final String ATAV_HOME = "/nfs/goldstein/software/atav_home/";
