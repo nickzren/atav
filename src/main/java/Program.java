0f2f77bac92194c0fc7deaa1297ae9e94a400f67
Nick <zhong.ren@hotmail.com>
2019-08-20 15:52:35 -0400

    disabled no use function

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -72,7 +72,7 @@ import function.genotype.family.FamilyCommand;
 import function.genotype.parental.ParentalCommand;
 import function.genotype.pedmap.PedMapCommand;
 import function.genotype.sibling.SiblingCommand;
-import function.genotype.statistics.LogisticRegression;
+//import function.genotype.statistics.LogisticRegression;
 import function.genotype.statistics.StatisticsCommand;
 import function.genotype.trio.TrioCommand;
 import function.genotype.var.ListVar;
@@ -161,9 +161,11 @@ public class Program {
                 runAnalysis(new FisherExactTest());
             } else if (StatisticsCommand.isLinear) {
                 runAnalysis(new LinearRegression());
-            } else if (StatisticsCommand.isLogistic) {
-                runAnalysis(new LogisticRegression());
-            } else if (FamilyCommand.isFamilyAnalysis) {
+            } 
+//            else if (StatisticsCommand.isLogistic) {
+//                runAnalysis(new LogisticRegression());
+//            }
+            else if (FamilyCommand.isFamilyAnalysis) {
                 runAnalysis(new FamilyAnalysis());
             } else if (SiblingCommand.isSiblingCompHet) {
                 runAnalysis(new ListSiblingComphet());
