5dd4278633d461cd57a7ebca952140ff3ab70fed
Nick <zhong.ren@hotmail.com>
2019-09-09 15:57:36 -0400

    add util function to convert mtr data

--- src/main/java/function/test/Test.java
+++ src/main/java/function/test/Test.java
@@ -34,7 +34,7 @@ public class Test extends AnalysisBase {
     @Override
     public void processDatabaseData() {
         try {
-            SplitDPBins.run();
+//            MTRConvert.run();  
         } catch (Exception ex) {
             Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         }
