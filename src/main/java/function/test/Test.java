48e65bc19c54b1414c49e29c1ef7fa523d967e80
Nick <zhong.ren@hotmail.com>
2019-09-17 16:25:31 -0400

    add script to convert db bin into 10 only format

--- src/main/java/function/test/Test.java
+++ src/main/java/function/test/Test.java
@@ -34,7 +34,7 @@ public class Test extends AnalysisBase {
     @Override
     public void processDatabaseData() {
         try {
-//            MTRConvert.run();  
+//            ConvertDPBins.run();  
         } catch (Exception ex) {
             Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         }
