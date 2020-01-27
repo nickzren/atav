a6d08aeb709116f4e426184d0b69786b07a29020
Nick <zhong.ren@hotmail.com>
2020-01-27 11:20:51 -0500

    minor updates to count variants

--- src/main/java/function/test/CountVariant.java
+++ src/main/java/function/test/CountVariant.java
@@ -4,6 +4,7 @@ import function.variant.base.RegionManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import utils.DBManager;
+import utils.LogManager;
 
 /**
  *
@@ -12,18 +13,26 @@ import utils.DBManager;
 public class CountVariant {
 
     public static void run() throws SQLException {
-        int count = 0;
+        long uniqueVarCount = 0;
+        long calledVarCount = 0;
 //            ConvertCalledVariant.run();
         for (String chr : RegionManager.ALL_CHR) {
-            String sql = "select count(distinct variant_id) count from WalDB.variant_chr" + chr + " "
-                    + "where effect_id = 43";
+            String sql = "select count(distinct variant_id) count from WalDB.variant_chr" + chr;
             ResultSet rset = DBManager.executeQuery(sql);
 
             if (rset.next()) {
-                count += rset.getInt("count");
+                uniqueVarCount += rset.getLong("count");
+            }
+            
+            sql = "select count(*) count from WalDB.called_variant_chr" + chr;
+            rset = DBManager.executeQuery(sql);
+
+            if (rset.next()) {
+                calledVarCount += rset.getLong("count");
             }
         }
 
-        System.out.println("total variant: " + count);
+        LogManager.writeAndPrint("total unique variant: " + uniqueVarCount);
+        LogManager.writeAndPrint("total called variant: " + calledVarCount);
     }
 }
