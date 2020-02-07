f45a81244a9fafc44d4d01c2c54b714e78d34a7e
Nick <zhong.ren@hotmail.com>
2020-02-07 12:49:07 -0500

    trunk

--- src/main/java/global/Data.java
+++ src/main/java/global/Data.java
@@ -8,7 +8,7 @@ public class Data {
 
     // software info
     public static final String APP_NAME = "ATAV (Analysis Tool for Annotated Variants)";
-    public static String VERSION = "7.1.2";
+    public static String VERSION = "trunk";
     public static String userName = System.getProperty("user.name");
 
     // atav home path
