90af2883c6dd341648aac629ac630dac52c71b7a
Nick <zhong.ren@hotmail.com>
2020-12-08 15:36:52 -0500

    gzip subrvis file

--- src/main/java/function/external/gnomad/GnomADManager.java
+++ src/main/java/function/external/gnomad/GnomADManager.java
@@ -6,7 +6,6 @@ import global.Data;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.sql.PreparedStatement;
--- src/main/java/function/external/rvis/RvisManager.java
+++ src/main/java/function/external/rvis/RvisManager.java
@@ -5,7 +5,6 @@ import global.Data;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.HashMap;
