c6f479ae0272f1b135970936c14c0c3feab95f47
Nick <zhong.ren@hotmail.com>
2021-07-15 11:04:36 -0400

    igm_af updated

--- src/main/java/function/external/igmaf/IGMAFManager.java
+++ src/main/java/function/external/igmaf/IGMAFManager.java
@@ -16,7 +16,7 @@ import utils.FormatManager;
  */
 public class IGMAFManager {
 
-    private static final String table = "igm_af.variant_042721";
+    private static final String table = "igm_af.variant_071421";
     private static PreparedStatement preparedStatement;
 
     public static void init() {
