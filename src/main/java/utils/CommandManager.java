b65871de0cc27bdf9eab4d60d4fde99625248b82
Nick <zhong.ren@hotmail.com>
2021-09-03 15:17:33 -0400

    disable checking on sex chr by default to diagnostic function

--- src/main/java/utils/CommandManager.java
+++ src/main/java/utils/CommandManager.java
@@ -353,6 +353,7 @@ public class CommandManager {
                     TrapCommand.isInclude = true;
                     MgiCommand.isInclude = true;
                     RevelCommand.isInclude = true;
+                    VariantLevelFilterCommand.disableCheckOnSexChr = true;
                     break;
                 case "--list-var-geno-lite":
                     VarGenoCommand.isListLite = true;
@@ -410,6 +411,7 @@ public class CommandManager {
                     TrapCommand.isInclude = true;
                     MgiCommand.isInclude = true;
                     RevelCommand.isInclude = true;
+                    VariantLevelFilterCommand.disableCheckOnSexChr = true;
                     break;
                 case "--list-parent-comp-het":
                     ParentCommand.isList = true;
