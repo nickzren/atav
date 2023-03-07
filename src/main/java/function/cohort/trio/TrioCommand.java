eb53025d6954565a22afb5ac99c528c58b922c00
Jaimee Beckett <jaimeebeckett15@gmail.com>
2023-03-07 11:01:46 -0500

    Added Phenolyzer to Trio

--- src/main/java/function/cohort/trio/TrioCommand.java
+++ src/main/java/function/cohort/trio/TrioCommand.java
@@ -1,6 +1,7 @@
 package function.cohort.trio;
 
 import java.util.Iterator;
+import static utils.CommandManager.getValidPath;
 import utils.CommandOption;
 
 /**
@@ -11,6 +12,8 @@ public class TrioCommand {
 
     public static boolean isList = false;
     public static boolean isExcludeDUO = false;
+    public static boolean isPhenolyzer = false;
+    public static String phenolyzerPhenotypePath = "";
     
     public static void initOptions(Iterator<CommandOption> iterator) {
         CommandOption option;
@@ -20,6 +23,10 @@ public class TrioCommand {
             switch (option.getName()) {
                 case "--exclude-duo":
                     isExcludeDUO = true;
+                    break;
+                case "--phenolyzer-ph":
+                    isPhenolyzer = true;
+                    phenolyzerPhenotypePath = getValidPath(option);
                     break;
                 default:
                     continue;
