6716a7c2503bb4656a5bec0528f26727683cca1d
Nick <zhong.ren@hotmail.com>
2023-06-02 16:53:05 -0400

    minor fix to calculate inheritance model

--- src/main/java/function/cohort/family/FamilyOutput.java
+++ src/main/java/function/cohort/family/FamilyOutput.java
@@ -46,17 +46,11 @@ public class FamilyOutput extends Output {
         return sj.toString();
     }
 
+    /*
+        dominate model: all cases are HET calls, all controls are HOM REF
+        recessive model: all cases are HOM calls, controls are either HET or REF
+     */
     public void calculateInheritanceModel(Family family) {
-        boolean control_ref = true; // All controls are REF
-        for (Sample sample : family.getControlList()) {
-            byte geno = getCalledVariant().getGT(sample.getIndex());
-
-            if (geno != Index.REF) {
-                control_ref = false;
-                break;
-            }
-        }
-
         boolean case_dom = true; // All cases are HET
         boolean case_rec = true; // All cases are HOM
         for (Sample sample : family.getCaseList()) {
@@ -71,13 +65,25 @@ public class FamilyOutput extends Output {
             }
         }
 
-        inheritanceModel = Data.STRING_NA;
-        if (control_ref) {
-            if (case_dom) {
-                inheritanceModel = "DOMINANT";
-            } else if (case_rec) {
-                inheritanceModel = "RECESSIVE";
+        boolean control_dom = true; // All controls are REF
+        boolean control_rec = true; // All controls are either REF or HET
+        for (Sample sample : family.getControlList()) {
+            byte geno = getCalledVariant().getGT(sample.getIndex());
+
+            if (geno != Index.REF) {
+                control_dom = false;
             }
+
+            if (geno != Index.REF && geno != Index.HET) {
+                control_rec = false;
+            }
+        }
+
+        inheritanceModel = Data.STRING_NA;
+        if (control_dom && case_dom) {
+            inheritanceModel = "DOMINANT";
+        } else if (control_rec && case_rec) {
+            inheritanceModel = "RECESSIVE";
         }
     }
 
